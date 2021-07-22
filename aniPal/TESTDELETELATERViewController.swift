245f16963b8e689e8f5da7b24e76fddec5cc79c5
Sky <estellasky@gmail.com>
2021-07-21 19:08:07 -0700

    aniPal 07/21/2021 edits kwk

---
+++ aniPal/TESTDELETELATERViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TESTDELETELATERViewController.swift
+//  aniPal
+//
+//  Created by Estella Sky Keyoung on 7/21/21.
+//
+
+import UIKit
+
+class TESTDELETELATERViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //wow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
