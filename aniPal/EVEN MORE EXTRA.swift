8eb1de63a4318dae7196313aa4f96a5190396d93
Sky <estellasky@gmail.com>
2021-07-21 19:20:33 -0700

    KWK PLEASE WORK 7/21

---
+++ aniPal/EVEN MORE EXTRA.swift
@@ -0,0 +1,29 @@
+//
+//  EVEN MORE EXTRA.swift
+//  aniPal
+//
+//  Created by Estella Sky Keyoung on 7/21/21.
+//
+
+import UIKit
+
+class EVEN_MORE_EXTRA: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//woo
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
