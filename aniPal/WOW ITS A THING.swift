ec785329912252d3ada4f20a923f2c1a95fcfb5d
Sky <estellasky@gmail.com>
2021-07-21 19:17:48 -0700

    KWK PLEASE WORK 7/21

---
+++ aniPal/WOW ITS A THING.swift
@@ -0,0 +1,29 @@
+//
+//  WOW ITS A THING.swift
+//  aniPal
+//
+//  Created by Estella Sky Keyoung on 7/21/21.
+//
+
+import UIKit
+
+class WOW_ITS_A_THING: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//yee
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
